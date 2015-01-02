626464e05809ef8024f16ff7e192a76bc09fca23
Adrian Kummerlaender <knairda@t-online.de>
2015-01-02 21:39:56 +0100

    Addded _Chicken-Scheme_ REPL config and shortcut

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,6 +4,7 @@ set EDITOR vim
 set PATH $PATH /usr/local/texlive/2014/bin/x86_64-linux
 
 alias ccat='pygmentize -f console256 -g'
+alias csi='csi -q'
 
 if status --is-login
     if test -z "$DISPLAY" -a $XDG_VTNR = 1
