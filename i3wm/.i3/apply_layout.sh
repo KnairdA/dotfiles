f733845de850997a1b28ecb011c8374762abecf1
Adrian Kummerlaender <knairda@t-online.de>
2015-05-26 21:26:25 +0200

    Added new "half_stack" layout and fixed apply script * newlines were not forwarded correctly and as such the script only supported a single layout

--- i3wm/.i3/apply_layout.sh
+++ i3wm/.i3/apply_layout.sh
@@ -2,7 +2,6 @@
 
 source ~/.dmenurc
 
-available_layouts=$(ls -A1 ~/.i3/layout/)
-selected_layout=$(echo $available_layouts | eval "dmenu $DMENU")
+selected_layout=$(ls -A1 ~/.i3/layout/ | eval "dmenu $DMENU")
 
 i3-msg "append_layout $(pwd)/.i3/layout/$selected_layout"
