29d7aac47c79b38a58579526ed7d4881a68fda8b
Adrian Kummerlaender <knairda@t-online.de>
2015-11-09 20:12:02 +0100

    Use `rofi` as layout selector instead of `dmenu`

--- i3wm/.i3/apply_layout.sh
+++ i3wm/.i3/apply_layout.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-source ~/.dmenurc
-
-selected_layout=$(ls -A1 ~/.i3/layout/ | eval "dmenu $DMENU")
+selected_layout=$(ls -A1 ~/.i3/layout/ | eval "rofi -dmenu -p 'Layout:'")
 
 i3-msg "append_layout $(pwd)/.i3/layout/$selected_layout"
