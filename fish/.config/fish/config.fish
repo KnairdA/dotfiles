2b61072239f5fe1a2cedbda082a2b3bce304c050
Adrian Kummerlaender <knairda@t-online.de>
2016-10-14 14:03:12 +0200

    Add desktop specific fish configuration

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,19 @@
 umask 0077
 
 set EDITOR vim
-set PATH $PATH /usr/local/bin/ /usr/local/texlive/2014/bin/x86_64-linux
 
 set fish_greeting ""
 
 alias csi='csi -q'
-alias i='sxiv'
 alias mp3-dl='youtube-dl --extract-audio --audio-format mp3'
 alias weather='curl wttr.in/Karlsruhe'
 
 eval (dircolors -c ~/.dir_colors)
 eval (python -m virtualfish)
+
+if [ (hostname) = "obelix" ]
+	set PATH $PATH /usr/local/bin/ /usr/local/texlive/2014/bin/x86_64-linux
+
+	alias i='sxiv'
+	alias orb='open ~/webarchive/data/(ls ~/webarchive/data/ | sort -R | head -n 1)/index.html'
+end
