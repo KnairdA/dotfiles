71f6a4fe010e6bcdcd1159d34dccda40aa7fe42d
Adrian Kummerlaender <knairda@t-online.de>
2016-12-03 21:32:16 +0100

    Add `arbtt` to autostart

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,7 +12,7 @@ eval (dircolors -c ~/.dir_colors)
 eval (python -m virtualfish)
 
 if [ (hostname) = "obelix" ]
-	set PATH $PATH /usr/local/bin/ /usr/local/texlive/2014/bin/x86_64-linux
+	set PATH $PATH /usr/local/bin/ /usr/local/texlive/2014/bin/x86_64-linux ~/.cabal/bin/
 
 	alias i='sxiv'
 	alias orb='open ~/webarchive/data/(ls ~/webarchive/data/ | sort -R | head -n 1)/index.html'
